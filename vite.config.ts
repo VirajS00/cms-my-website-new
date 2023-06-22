62d3342cfc4dff1a997cb17bfe13b24d76a0327d
Viraj Shukla <virajshukla00@gmail.com>
2023-06-22 13:46:30 +0530

    added vercel adapter

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
 import solid from "solid-start/vite";
 import { defineConfig } from "vite";
 import dotenv from "dotenv";
+import vercel from "solid-start-vercel";
 
 export default defineConfig(() => {
-	dotenv.config();
+  dotenv.config();
 
-	return {
-		plugins: [solid()],
-	};
+  return {
+    plugins: [
+      solid({
+        adapter: vercel({
+          edge: false,
+        }),
+      }),
+    ],
+  };
 });
